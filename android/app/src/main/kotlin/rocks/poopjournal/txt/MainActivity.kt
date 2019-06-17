39e0841fbba3c3af73d271bc2decae91668a48eb
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-06-17 16:09:04 +0200

    Create Flutter app

---
+++ android/app/src/main/kotlin/rocks/poopjournal/txt/MainActivity.kt
@@ -0,0 +1,11 @@
+package rocks.poopjournal.txt
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
