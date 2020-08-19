a4a8f689a2c40c1228f06354199c11669b4582ba
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2020-08-19 21:20:43 +0200

    Add icon font

---
+++ lib/widget/txt_icons.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/widgets.dart';
+
+@immutable
+class TxtIcons {
+  TxtIcons._();
+
+  static const _fontFamily = 'Txt';
+
+  static const IconData add = IconData(0xe800, fontFamily: _fontFamily);
+  static const IconData archive = IconData(0xe801, fontFamily: _fontFamily);
+  static const IconData back = IconData(0xe802, fontFamily: _fontFamily);
+  static const IconData blank = IconData(0xe803, fontFamily: _fontFamily);
+  static const IconData document = IconData(0xe804, fontFamily: _fontFamily);
+  static const IconData drawer = IconData(0xe805, fontFamily: _fontFamily);
+  static const IconData edit = IconData(0xe806, fontFamily: _fontFamily);
+  static const IconData feedback = IconData(0xe80d, fontFamily: _fontFamily);
+  static const IconData fileMarkdown =
+      IconData(0xe808, fontFamily: _fontFamily);
+  static const IconData fileText = IconData(0xe80e, fontFamily: _fontFamily);
+  static const IconData filter = IconData(0xe80f, fontFamily: _fontFamily);
+  static const IconData formatBold = IconData(0xe810, fontFamily: _fontFamily);
+  static const IconData formatCode = IconData(0xe811, fontFamily: _fontFamily);
+  static const IconData formatHeader =
+      IconData(0xe812, fontFamily: _fontFamily);
+  static const IconData formatImage = IconData(0xe813, fontFamily: _fontFamily);
+  static const IconData formatItalic =
+      IconData(0xe814, fontFamily: _fontFamily);
+  static const IconData formatLink = IconData(0xe815, fontFamily: _fontFamily);
+  static const IconData formatList = IconData(0xe816, fontFamily: _fontFamily);
+  static const IconData formatListNumbered =
+      IconData(0xe817, fontFamily: _fontFamily);
+  static const IconData formatQuote = IconData(0xe818, fontFamily: _fontFamily);
+  static const IconData formatStrikethrough =
+      IconData(0xe819, fontFamily: _fontFamily);
+  static const IconData formatTable = IconData(0xe81a, fontFamily: _fontFamily);
+  static const IconData information = IconData(0xe81b, fontFamily: _fontFamily);
+  static const IconData label = IconData(0xe81c, fontFamily: _fontFamily);
+  static const IconData more = IconData(0xe81d, fontFamily: _fontFamily);
+  static const IconData order = IconData(0xe81e, fontFamily: _fontFamily);
+  static const IconData pdf = IconData(0xe81f, fontFamily: _fontFamily);
+  static const IconData preview = IconData(0xe820, fontFamily: _fontFamily);
+  static const IconData search = IconData(0xe821, fontFamily: _fontFamily);
+  static const IconData settings = IconData(0xe822, fontFamily: _fontFamily);
+  static const IconData share = IconData(0xe823, fontFamily: _fontFamily);
+  static const IconData sortAz = IconData(0xe825, fontFamily: _fontFamily);
+  static const IconData sortTime = IconData(0xe826, fontFamily: _fontFamily);
+  static const IconData theme = IconData(0xe827, fontFamily: _fontFamily);
+  static const IconData themeAuto = IconData(0xe829, fontFamily: _fontFamily);
+  static const IconData themeDark = IconData(0xe82a, fontFamily: _fontFamily);
+  static const IconData themeLight = IconData(0xe82b, fontFamily: _fontFamily);
+  static const IconData themePurple = IconData(0xe82f, fontFamily: _fontFamily);
+  static const IconData trash = IconData(0xe82d, fontFamily: _fontFamily);
+}
