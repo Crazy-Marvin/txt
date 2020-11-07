d10e95a2b83d291614e4a67ea71b26c28a232dbf
Jan Heinrich Reimer <heinrich@reimer.family>
2020-11-07 15:14:43 +0100

    Add draft Dropbox sync service

---
+++ lib/sync/dropbox/dropbox_sync.dart
@@ -0,0 +1,32 @@
+import 'package:dropbox_client/dropbox_client.dart';
+import 'package:flutter/widgets.dart';
+import 'package:txt/sync/file.dart';
+import 'package:txt/sync/sync.dart';
+
+class DropboxSyncService implements SyncService {
+  String token;
+
+  @override
+  Future<void> authorize(BuildContext context) async {
+    // TODO store access token and try authenticating with access token first.
+    await Dropbox.authorize();
+    token = await Dropbox.getAccessToken();
+  }
+
+  @override
+  Future<String> getAccountName() => Dropbox.getAccountName();
+
+  @override
+  Future<List<RemoteFile>> listFolder(String path) async {
+    var files = await Dropbox.listFolder(path);
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> download(String remotePath, String localPath) =>
+      Dropbox.download(remotePath, localPath);
+
+  @override
+  Future<void> upload(String localPath, String remotePath) =>
+      Dropbox.upload(localPath, remotePath);
+}
